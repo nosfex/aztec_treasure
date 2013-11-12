2a2730ff14b09100e4630d37d5dd11f10b49afea
Gerardo Heidel <heidel.gerardo@gmail.com>
2013-11-12 04:03:57 -0200

    BSP fixes

--- Assets/scripts/Room.cs
+++ Assets/scripts/Room.cs
@@ -9,15 +9,28 @@ public class Room
 
 	public GameObject roomHolder;	
 	public GameObject[,] tiles;
-
+	int width;
+	int height;
 	
 	public Room(int w, int h)
 	{
-	
+		width = w;
+		height = h;
 		tiles = new GameObject[w, h];
 		//Start();
 	}
 	
+	public void cutRoom(GameObject tiles)
+	{
+		for(int i = 0 ; i < width; i++)
+		{
+			for(int j = 0; j <height; j++)
+			{
+				//MonoBehaviour.Destroy(tiles[i]
+				
+			}	
+		}
+	}
 	public void Start () 
 	{
 	
