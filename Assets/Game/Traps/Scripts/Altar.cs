ce0e2fc077beb2297d7e8a18dcfc4a0c83b40268
CreativeVomit <black.pentagram@gmail.com>
2013-12-08 14:32:43 -0300

    Added altar

---
+++ Assets/Game/Traps/Scripts/Altar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Altar : MonoBehaviour 
+{
+
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	private float currencyCooldown;
+	private bool activated = false;
+	
+	public float maxCurrencyCooldown = 1.0f;
+		
+	void Update() 
+	{
+		AztecPlayer p = (AztecPlayer)GameDirector.i.playerLeft;
+		
+		if ( activated )
+		{
+			currencyCooldown += Time.deltaTime;
+	
+			if(currencyCooldown >= maxCurrencyCooldown )
+			{
+				currencyCooldown = 0.0f;
+				p.trapCurrency += 10;
+			}
+		}
+	}
+}
