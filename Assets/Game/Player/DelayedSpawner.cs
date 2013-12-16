9ff98158048338652795e2172a26d0814b90f998
Gerardo Heidel <gerardo@pixowl.com>
2013-12-16 18:00:27 -0300

    gh; Roar

---
+++ Assets/Game/Player/DelayedSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
