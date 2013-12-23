c25bcc9f22d598a78086ccd429a52e60f3a5aaea
Brian Amadori <brian@pixowl.com>
2013-12-23 18:33:59 -0300

    Skelly explosion.

---
+++ Assets/Game/Props/ExplosionSkelly.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionSkelly : MonoBehaviour 
+{
+	public GameObject[] prefabDebris;
+
+	void Start () 
+	{
+		int partIndex = 1;
+		foreach ( GameObject go in prefabDebris )
+		{
+			GameObject instance = (GameObject)Instantiate( go, transform.position + (Vector3.up * 0.4f), Quaternion.identity );
+			BaseObject bo = instance.GetComponentInChildren<BaseObject>();
+			float force = 0.025f;
+			bo.velocity.x = Random.Range ( -force, force );
+			bo.velocity.z = Random.Range ( -force, force );
+			bo.gravity.y = -0.01f;
+			
+			SpriteAnimator spr = instance.GetComponentInChildren<SpriteAnimator>();
+			spr.startingAnimationName = "Part" + partIndex;
+			partIndex++;
+			
+			instance.transform.parent = transform;
+		}
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
