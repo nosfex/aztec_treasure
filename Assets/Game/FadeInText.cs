0e65950ffd2f9a8e4b250bead5d93713cad901b4
CreativeVomit <black.pentagram@gmail.com>
2014-01-02 22:39:41 -0300

    GUI opening scripts.

---
+++ Assets/Game/FadeInText.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInText : MonoBehaviour 
+{
+
+	public float delay;
+	public float duration;
+	
+	float delayTimer;
+	float progressTimer;
+	
+	void Awake () 
+	{
+		foreach ( Renderer r in GetComponentsInChildren<Renderer>() )
+			r.enabled = false;
+		
+		foreach ( TextMesh t in GetComponentsInChildren<TextMesh>() )
+			t.color = new Color( 1.0f, 1.0f, 1.0f, 0 );
+					
+		
+		delayTimer = delay;
+		progressTimer = duration;
+	}
+	
+	void Update () 
+	{
+		if ( delayTimer > 0 )
+		{
+			delayTimer -= Time.deltaTime;
+	
+			if ( delayTimer <= 0 )
+			{
+				foreach ( Renderer r in GetComponentsInChildren<Renderer>() )
+					r.enabled = true;
+			}
+		}
+		else 
+		{
+			progressTimer -= Time.deltaTime;
+			
+			if ( progressTimer > 0 )
+			{
+				foreach ( TextMesh t in GetComponentsInChildren<TextMesh>() )
+					t.color = new Color( 1.0f, 1.0f, 1.0f, 1.0f - (progressTimer / duration) );
+			}
+			else 
+			{
+				foreach ( TextMesh t in GetComponentsInChildren<TextMesh>() )
+					t.color = new Color( 1.0f, 1.0f, 1.0f, 1.0f );
+				
+				enabled = false;
+			}
+			
+		}
+	
+	}
+}
---
+++ Assets/Game/TimeLabel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeLabel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		iTween.MoveFrom( gameObject, iTween.Hash( "oncomplete", "OnFadeInComplete", "y", transform.position.y + 2.0f, "time", .5f, "easetype", iTween.EaseType.easeOutQuad ) );
+		iTween.FadeFrom( gameObject, iTween.Hash( "alpha", 0, "time", 1.0f ) );
+	
+	}
+	
+	void OnFadeInComplete()
+	{
+		iTween.FadeTo ( gameObject, iTween.Hash ( "delay", 1.5f, "alpha", 0, "time", 1.0f, "oncomplete", "OnFadeInEnd") );
+	}
+	
+	void OnFadeInEnd()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
