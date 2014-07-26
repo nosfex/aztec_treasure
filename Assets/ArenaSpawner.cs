0a4285527e45b4c478e8690f8ac1a55b539372cb
Gerardo Heidel <heidel.gerardo@gmail.com>
2014-07-26 17:23:09 -0300

    GH: Arena level

---
+++ Assets/ArenaSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArenaSpawner : MonoBehaviour {
+	
+	
+	public BaseObject[] enemiesToSpawn; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnTriggerExit() 
+	{
+		
+		print("CULO");
+	}
+}
