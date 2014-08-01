a98a034b66436b2a6025f77a8f826f94181da200
Gerardo Heidel <heidel.gerardo@gmail.com>
2014-08-01 07:26:54 -0300

    sad

---
+++ Assets/SpikeWall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		print("WALL IS COLLIDING");
+		Player p = collision.gameObject.GetComponent<Player>();
+		if(p != null)
+		{
+			p.OnHit(gameObject);
+			
+		}
+	}
+	
+}
