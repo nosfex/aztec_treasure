bc4bdfa42a47c10a38b00b59c40cd236716550cc
Gerardo Heidel <heidel.gerardo@gmail.com>
2014-08-10 18:00:13 -0300

    GH: Added the target for the fireballs

---
+++ Assets/LifeSpan.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeSpan : MonoBehaviour {
+	
+	public float maxLifeSpan = 1.2f;
+	float currentSpan = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentSpan += Time.deltaTime;
+		if(currentSpan >= maxLifeSpan)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
